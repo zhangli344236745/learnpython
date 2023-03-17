4bb3e1f11342fa75dea086f75d08366e795ecfb6
mooseki <344236745@qq.com>
2023-03-17 23:39:05 +0800

    search learn2

--- search/generic_search.py
+++ search/generic_search.py
@@ -1,12 +1,100 @@
+from __future__ import annotations
 from typing import Protocol,Iterable,TypeVar,Sequence,Generic,List,Callable,Set,Deque,Dict,Any,Optional
 from heapq import heappush,heappop
 
 T = TypeVar("T")
+
 def linear_contains(iterable: Iterable[T],key: T) -> bool:
     for item in iterable:
         if item == key:
             return True
     return False
+
+class Stack(Generic[T]):
+    def __init__(self) -> None:
+        self._container: List[T] = []
+
+    @property
+    def empty(self) -> bool:
+        return not self._container
+
+    def push(self,item: T) -> None:
+        self._container.append(item)
+
+    def pop(self) -> T:
+        return self._container.pop()
+
+    def __repr__(self) -> str:
+        return repr(self._container)
+
+class Node(Generic[T]):
+    def __init__(self , state:T, parant: Optional[Node],cost: float = 0.0,heuristic: float = 0.0) -> None:
+        self.state = state
+        self.parent:Optional[Node] = parant
+        self.cost: float = cost
+        self.heuritic = heuristic
+
+    def __lt__(self, other:Node) -> bool:
+        return (self.cost + self.heuritic)< (other.cost + other.heuritic)
+
+def dfs(initial: T,goal_test:Callable[[T],bool],successors:Callable[[T],List[T]]) -> Optional[Node[T]]:
+    frontier : Stack[Node[T]] = Stack()
+    frontier.push(Node(initial,None))
+    explored: Set[T] = {initial}
+    while not frontier.empty:
+        current_node: Node[T] = frontier.pop()
+        current_state: T = current_node.state
+        if goal_test(current_state):
+            return current_node
+        for child in successors(current_state):
+            if child in explored:
+                continue
+            explored.add(child)
+            frontier.push(Node(child,current_node))
+    return None
+
+class Queue(Generic[T]):
+    def __init__(self) -> None:
+        self.__container: Deque[T] = Deque()
+    @property
+    def empty(self) -> bool:
+        return not self.__container
+
+    def pop(self) -> T:
+        return self.__container.popleft()
+
+    def push(self,item:T) -> None:
+        self.__container.append(item)
+
+    def __repr__(self) -> str:
+        return repr(self.__container)
+
+def bfs(inital:T,goal_test:Callable[[T],bool],successors:Callable[[T],List[T]]) -> Optional[Node[T]]:
+    frozenser: Queue[Node[T]] = Queue()
+    frozenser.push(Node(inital,None))
+    explored :Set[T] = {inital}
+    while not frozenser.empty:
+        current_node:Node[T] = frozenser.pop()
+        current_state: T = current_node.state
+
+        if goal_test(current_state):
+            return current_node
+
+        for child in successors(current_state):
+            if child in explored:
+                continue
+            explored.add(child)
+            frozenser.push(Node(child,current_node))
+    return None
+
+def node_to_path(node:None[T]) -> List[T]:
+    path: List[T] = [node.state]
+    while node.parent is not None:
+        node = node.parent
+        path.append(node.state)
+    path.reverse()
+    return path
+
 
 C = TypeVar("C",bound="Comparable")
 class Comparable(Protocol):
@@ -14,6 +102,7 @@ class Comparable(Protocol):
         return (not self < other) and self != other
 
     def __le__(self:C, other:C) -> bool:
+        print("sourt:{}".format(C))
         return self < other or self == other
 
     def __ge__(self:C, other:C) -> bool:
@@ -30,11 +119,12 @@ def binary_contains(sequence:Sequence[C],key:C) -> bool:
             hight = mid - 1
         else:
             return True
-    return True
+    return False
 
 
 if __name__ == "__main__":
     print(linear_contains([1,5,15,15,15,20],5))
     print(binary_contains(["a","d","e","f","z"],"f"))
+    print(binary_contains(["john","mark","ronald","sarah","abby"],"abby"))
 
 
---
+++ search/maze.py
@@ -0,0 +1,85 @@
+from __future__ import annotations
+from enum import Enum
+from typing import List,NamedTuple,Callable,Optional
+import random
+from search.generic_search import Node,dfs,node_to_path,bfs
+from math import sqrt
+
+class Cell(str,Enum):
+    EMPTY = "E"
+    BLOCKED = "X"
+    START = "S"
+    GOAL = "G"
+    PATH = "*"
+
+class MazeLocation(NamedTuple):
+    row: int
+    column: int
+
+class Maze:
+    def __init__(self, rows: int = 10, coloumns: int = 10,sparsenenss: float = 0.2, start: MazeLocation = MazeLocation(0,0), goal: MazeLocation = MazeLocation(9,9)) -> None:
+        self._rows: int = rows
+        self._cloumns: int = coloumns
+        self.start: MazeLocation = start
+        self.goal: MazeLocation = goal
+        self._grid: List[List[Cell]] = [[Cell.EMPTY for c in range(coloumns)] for r in range(rows)]
+        self._randomly_fill(rows,coloumns,sparsenenss)
+        self._grid[start.row][start.column] = Cell.START
+        self._grid[goal.row][goal.column] = Cell.GOAL
+
+    def _randomly_fill(self,rows: int ,columns: int,sparseness:float):
+        for row in range(rows):
+            for column in range(columns):
+                if random.uniform(0,1.0) < sparseness:
+                    self._grid[row][column] = Cell.BLOCKED
+
+    def goal_test(self,m1: MazeLocation) -> bool:
+        return m1 == self.goal
+
+    def successors(self,m1:MazeLocation) -> List[MazeLocation]:
+        locations: List[MazeLocation] = []
+        if m1.row + 1 < self._rows and self._grid[m1.row + 1][m1.column] != Cell.BLOCKED:
+            locations.append(MazeLocation(m1.row+1,m1.column))
+        if m1.row - 1 >= 0 and self._grid[m1.row - 1][m1.column] != Cell.BLOCKED:
+            locations.append(MazeLocation(m1.row-1,m1.column))
+        if m1.column + 1 < self._cloumns and self._grid[m1.row][m1.column +1] != Cell.BLOCKED:
+            locations.append(MazeLocation(m1.row, m1.column+1))
+        if m1.column - 1 > 0 and self._grid[m1.row][m1.column - 1] != Cell.BLOCKED:
+            locations.append(MazeLocation(m1.row, m1.column - 1))
+        return locations
+
+    def mark(self,path:List[MazeLocation]):
+        for maze_location in path:
+            self._grid[maze_location.row][maze_location.column] = Cell.PATH
+        self._grid[self.start.row][self.start.column] = Cell.START
+        self._grid[self.goal.row][self.goal.column] = Cell.GOAL
+
+    def __str__(self) -> str:
+        output: str = ""
+        for row in self._grid:
+            output += "".join([c.value for c in row]) + "\n"
+        return output
+
+def euclidean_distance(goal:MazeLocation) -> Callable[[MazeLocation],float]:
+    def distance(m1:MazeLocation) -> float:
+        xdist: int = m1.column - goal.column
+        ydist: int = m1.row - goal.row
+        return sqrt((xdist*xdist)+(ydist*ydist))
+    return distance
+
+
+if __name__ == "__main__":
+    maze: Maze = Maze()
+    print(maze)
+    #solution1:Optional[Node[MazeLocation]] = dfs(maze.start,maze.goal_test,maze.successors)
+    solution1: Optional[Node[MazeLocation]] = bfs(maze.start, maze.goal_test, maze.successors)
+    if solution1 is None:
+        print("no soluation found")
+    else:
+        print("found")
+        path1:List[MazeLocation] = node_to_path(solution1)
+        maze.mark(path1)
+        print(maze)
+
+
+
